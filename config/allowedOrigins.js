c5694a51f348663619a4bc074e15792c7b9bc2cb
Vitaliy Galchinskiy <vitaliy.galchinskiy@jointoit.com>
2024-10-14 21:26:54 +0300

    add logger

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrgigins = [
+  "http://localhost:3000",
+  "https://www.ddnf.cofm",
+  "https://ddnf.cofm",
+];
+
+module.exports = allowedOrgigins;
---
+++ config/corsOptions.js
@@ -0,0 +1,15 @@
+const allowedOrgigins = require("./allowedOrigins");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrgigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
